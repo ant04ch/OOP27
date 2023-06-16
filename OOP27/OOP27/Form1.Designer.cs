e3f3ad99809edafd5342b2702c98ae4258f4b622
ant04ch <antoniolrr81@gmail.com>
2023-06-16 17:29:51 +0300

    Anton

---
+++ OOP27/OOP27/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace OOP27
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton2,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1002, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.treeView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.treeView1.Location = new System.Drawing.Point(0, 25);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(154, 425);
+            this.treeView1.TabIndex = 7;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listView1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(154, 25);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(655, 425);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label1.Location = new System.Drawing.Point(815, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label2.Location = new System.Drawing.Point(815, 404);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label3.Location = new System.Drawing.Point(815, 417);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label4.Location = new System.Drawing.Point(815, 430);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DimGray;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Control;
+            this.textBox1.Location = new System.Drawing.Point(154, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(614, 20);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.DimGray;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.Location = new System.Drawing.Point(815, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 20);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Location = new System.Drawing.Point(954, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "->";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.Location = new System.Drawing.Point(774, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "->";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::OOP27.Properties.Resources.icons8_назад_60;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::OOP27.Properties.Resources.icons8_вперед_60;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1002, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Провідник";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ OOP27/OOP27/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace OOP27
+{
+    public partial class Form1 : Form
+    {
+        private string currentDirectory;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+            label4.Text = "";
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode selectedNode = e.Node;
+
+            if (selectedNode != null)
+            {
+                string selectedPath = selectedNode.FullPath;
+
+                if (Directory.Exists(selectedPath))
+                {
+                    currentDirectory = selectedPath;
+                    LoadDirectories(selectedPath, selectedNode);
+                    LoadFiles(selectedPath);
+                }
+            }
+            if (selectedNode != null)
+            {
+                string selectedPath = selectedNode.FullPath;
+                textBox1.Text = selectedPath; // Оновлення значення textBox1 з вибраним шляхом
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                string selectedFileName = selectedItem.Text;
+                string selectedFilePath = Path.Combine(currentDirectory, selectedFileName);
+
+                if (File.Exists(selectedFilePath))
+                {
+                    FileInfo fileInfo = new FileInfo(selectedFilePath);
+                    ShowFileProperties(fileInfo);
+                }
+            }
+        }
+
+        private void LoadDrives()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                TreeNode driveNode = new TreeNode(drive.Name);
+                driveNode.ImageIndex = 0;
+
+                treeView1.Nodes.Add(driveNode);
+            }
+        }
+
+        private void LoadDirectories(string path, TreeNode parentNode)
+        {
+            try
+            {
+                if (!Directory.Exists(path))
+                    return;
+
+                DirectoryInfo directory = new DirectoryInfo(path);
+
+                // Очищення попередніх підтек та файлів
+                parentNode.Nodes.Clear();
+                listView1.Items.Clear();
+                imageList1.Images.Clear();
+
+                // Додавання батьківської теки до ListView
+                if (parentNode.Parent != null)
+                {
+                    ListViewItem parentItem = new ListViewItem("..");
+                    parentItem.ImageIndex = 1; // Індекс іконки теки
+                    listView1.Items.Add(parentItem);
+                }
+
+                int imageIndex = 2; // Початковий індекс для іконок файлів
+
+                foreach (DirectoryInfo subDirectory in directory.GetDirectories())
+                {
+                    TreeNode directoryNode = new TreeNode(subDirectory.Name);
+                    directoryNode.ImageIndex = 1; // Індекс іконки теки
+
+                    parentNode.Nodes.Add(directoryNode);
+
+                    // Додавання теки до ListView
+                    ListViewItem item = new ListViewItem(subDirectory.Name);
+                    item.ImageIndex = 1; // Індекс іконки теки у списку зображень
+
+                    listView1.Items.Add(item);
+                }
+
+                foreach (FileInfo file in directory.GetFiles())
+                {
+                    Icon fileIcon = Icon.ExtractAssociatedIcon(file.FullName);
+                    imageList1.Images.Add(fileIcon);
+
+                    // Додавання файлу до ListView з відповідною іконкою
+                    ListViewItem item = new ListViewItem(file.Name);
+                    item.SubItems.Add(file.Length.ToString());
+                    item.SubItems.Add(file.LastWriteTime.ToString());
+                    item.ImageIndex = imageIndex; // Встановлення індексу зображення
+
+                    listView1.Items.Add(item);
+
+                    imageIndex++; // Інкремент індексу зображення для наступного файлу
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void LoadFiles(string path)
+        {
+            try
+            {
+                DirectoryInfo directory = new DirectoryInfo(path);
+
+                listView1.Items.Clear();
+                imageList1.Images.Clear();
+
+                int imageIndex = 0;
+
+                foreach (FileInfo file in directory.GetFiles())
+                {
+                    Icon fileIcon = Icon.ExtractAssociatedIcon(file.FullName);
+                    imageList1.Images.Add(fileIcon);
+                    ListViewItem item = new ListViewItem(file.Name);
+                    item.SubItems.Add(FormatSize(file.Length));
+                    item.SubItems.Add(file.LastWriteTime.ToString());
+                    item.ImageIndex = imageIndex; 
+
+                    listView1.Items.Add(item);
+
+                    imageIndex++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading files: " + ex.Message);
+            }
+        }
+
+        private void ShowFileProperties(FileInfo fileInfo)
+        {
+            label1.Text = fileInfo.Name;
+            label2.Text = "Створено: " + fileInfo.CreationTime.ToString();
+            label3.Text = "Оновлено: " + fileInfo.LastWriteTime.ToString();
+            label4.Text = "Розмір: " + FormatSize(fileInfo.Length);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            treeView1.AfterSelect += treeView1_AfterSelect;
+            LoadDrives();
+
+            listView1.LargeImageList = imageList1;
+            button1.Click += button1_Click;
+            button2.Click += button2_Click;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = textBox1.Text;
+
+            if (Directory.Exists(path))
+            {
+                currentDirectory = path;
+                LoadDirectories(path, null); 
+                LoadFiles(path);
+            }
+            else
+            {
+                MessageBox.Show("Вказано недійсний шлях до папки.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string searchTerm = textBox2.Text;
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                List<string> matchingFiles = SearchFiles(currentDirectory, searchTerm);
+
+                if (matchingFiles.Count > 0)
+                {
+                    // Очищаємо ListView та imageList1
+                    listView1.Items.Clear();
+                    imageList1.Images.Clear();
+
+                    foreach (string file in matchingFiles)
+                    {
+                        // Додаємо знайдений файл до ListView
+                        ListViewItem item = new ListViewItem(file);
+                        item.SubItems.Add(""); // Порожнє значення для додаткових стовпців (розмір, дата)
+
+                        // Отримуємо іконку файлу
+                        Icon fileIcon = Icon.ExtractAssociatedIcon(file);
+                        imageList1.Images.Add(fileIcon);
+
+                        // Встановлюємо індекс зображення для елементу
+                        item.ImageIndex = imageList1.Images.Count - 1;
+
+                        listView1.Items.Add(item);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Файли зі словом \"" + searchTerm + "\" не знайдені.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введіть слово для пошуку.");
+            }
+        }
+        private List<string> SearchFiles(string path, string searchTerm)
+        {
+            List<string> matchingFiles = new List<string>();
+
+            try
+            {
+                DirectoryInfo directory = new DirectoryInfo(path);
+
+                foreach (FileInfo file in directory.GetFiles("*.*", SearchOption.AllDirectories))
+                {
+                    if (file.Name.Contains(searchTerm))
+                    {
+                        matchingFiles.Add(file.FullName);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка під час пошуку файлів: " + ex.Message);
+            }
+
+            return matchingFiles;
+        }
+
+        private string FormatSize(long size)
+        {
+            const long KB = 1024;
+            const long MB = KB * 1024;
+            const long GB = MB * 1024;
+
+            string sizeText;
+
+            if (size >= GB)
+            {
+                double sizeInGB = (double)size / GB;
+                sizeText = string.Format("{0:0.00} ГБ", sizeInGB);
+            }
+            else if (size >= MB)
+            {
+                double sizeInMB = (double)size / MB;
+                sizeText = string.Format("{0:0.00} МБ", sizeInMB);
+            }
+            else if (size >= KB)
+            {
+                double sizeInKB = (double)size / KB;
+                sizeText = string.Format("{0:0.00} КБ", sizeInKB);
+            }
+            else
+            {
+                sizeText = size.ToString() + " байт";
+            }
+
+            return sizeText;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (treeView1.SelectedNode != null && treeView1.SelectedNode.Parent != null)
+            {
+                treeView1.SelectedNode = treeView1.SelectedNode.Parent;
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (treeView1.SelectedNode != null && treeView1.SelectedNode.Nodes.Count > 0)
+            {
+                treeView1.SelectedNode = treeView1.SelectedNode.Nodes[0];
+            }
+        }
+    }
+}
